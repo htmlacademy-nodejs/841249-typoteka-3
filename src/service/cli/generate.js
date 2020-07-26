e4bfb9184775f7975a2796364a5f1240cf0b7cf8
Dmitry <spirin2205@yandex.ru>
2020-07-26 15:02:27 +0300

    added async reading files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,61 +1,27 @@
 'use strict';
-const fs = require(`fs`);
+const fs = require(`fs`).promises;
+
 const {
   getRandomInt,
 } = require(`../../utils`);
-
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mock.json`;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
+const FILES_PATHES = {
+  titlesPath: `data/titles.txt`,
+  announcesPath: `data/sentences.txt`,
+  categoriesPath: `data/categories.txt`,
+};
 
-const ANNOUNCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const readFiles = async (path) => {
+  try {
+    const content = await fs.readFile(path, `utf8`);
+    return content.split(`\n`).filter(Boolean);
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
 
 const getRandomDate = () =>{
   const MOUNCE_DEEP = 3;
@@ -76,32 +42,35 @@ const getRandomDate = () =>{
   return new Date(`${randomMounth}/${randomDay}/${currentyear}`).toLocaleString();
 };
 
-const getText = (maxLength = 5) => {
+const getText = (announces, maxLength = 5) => {
   const MIN_LENGTH = 1;
   const counter = getRandomInt(MIN_LENGTH, maxLength);
   let announce = ``;
   for (let i = 0; i <= counter; i++) {
-    announce += ANNOUNCES[getRandomInt(0, ANNOUNCES.length)];
+    announce += announces[getRandomInt(0, announces.length)];
   }
   return announce;
 };
 
-const generateOffers = (count) => (
+const generateOffers = (count, titles, categories, announces) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length)],
+    title: titles[getRandomInt(0, titles.length)],
     createdDate: getRandomDate(),
-    announce: getText(),
-    fullText: getText(ANNOUNCES.length),
-    сategory: CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)],
+    announce: getText(announces),
+    fullText: getText(announces, announces.length),
+    сategory: categories[getRandomInt(0, categories.length - 1)],
   }))
 );
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+  async run(args) {
+    const titles = await readFiles(FILES_PATHES.titlesPath);
+    const announces = await readFiles(FILES_PATHES.announcesPath);
+    const categories = await readFiles(FILES_PATHES.categoriesPath);
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, announces));
     fs.writeFile(FILE_NAME, content, (err) => {
       if (err) {
         return console.error(1);
