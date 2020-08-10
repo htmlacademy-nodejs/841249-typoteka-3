4ba66d6a6e5fbe5b9a7d4bb6effef97a031b4b8e
Dmitriy <spirin2205@yandex.ru>
2020-08-10 23:28:41 +0300

    install express and sets routes

---
+++ src/express/routes/categories.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const {Router} = require(`express`);
+
+const categoriesRouter = new Router();
+
+categoriesRouter.get(`/`, (req, res) => res.send(`/categories`));
+
+module.exports = categoriesRouter;
